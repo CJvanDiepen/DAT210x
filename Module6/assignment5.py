a51e9598fb5a98f8bd2a52de1a7c70ec3980bb11
CJvanDiepen <sjaak.vandiepen@tno.nl>
2017-05-24 17:55:29 +0200

    Add work on decision trees.

--- Module6/assignment5.py
+++ Module6/assignment5.py
@@ -12,9 +12,12 @@ import pandas as pd
 # Check NA Encoding
 #
 # .. your code here ..
+names = ['class', 'cap-shape', 'cap-surface', 'cap-color', 'bruises', 'odor', 'gill-attachment', 'gill-spacing', 'gill-size', 'gill-color', 'stalk-shape', 'stalk-root', 'stalk-surface-above-ring', 'stalk-surface-below-ring', 'stalk-color-above-ring', 'stalk-color-below-ring', 'veil-type', 'veil-color', 'ring-number', 'ring-type', 'spore-print-color', 'population', 'habitat']
+X = pd.read_csv(r"C:\Users\diepencjv\repos\DAT210x\Module6\Datasets\agaricus-lepiota.data", names=names)
+
 
 # INFO: An easy way to show which rows have nans in them
-#print X[pd.isnull(X).any(axis=1)]
+print X[pd.isnull(X).any(axis=1)]
 
 
 # 
@@ -23,20 +26,21 @@ import pandas as pd
 # .. your code here ..
 print X.shape
 
-
 #
 # TODO: Copy the labels out of the dset into variable 'y' then Remove
 # them from X. Encode the labels, using the .map() trick we showed
 # you in Module 5 -- canadian:0, kama:1, and rosa:2
 #
 # .. your code here ..
-
+y = X['class']
+X = X.drop('class', axis=1)
+y = y.map({'p':0, 'e':1})
 
 #
 # TODO: Encode the entire dataset using dummies
 #
 # .. your code here ..
-
+X = pd.get_dummies(X)
 
 # 
 # TODO: Split your data into test / train sets
@@ -44,24 +48,32 @@ print X.shape
 # Use variable names: X_train, X_test, y_train, y_test
 #
 # .. your code here ..
+from sklearn.model_selection import train_test_split
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.3, random_state=7)
 
 
 #
 # TODO: Create an DT classifier. No need to set any parameters
 #
 # .. your code here ..
+from sklearn.tree import DecisionTreeClassifier
 
+dt = DecisionTreeClassifier(random_state=2)
  
 #
 # TODO: train the classifier on the training data / labels:
 # TODO: score the classifier on the testing data / labels:
 #
 # .. your code here ..
+dt.fit(X_train, y_train)
+score = dt.score(X_test, y_test)
+
+
 print "High-Dimensionality Score: ", round((score*100), 3)
 
 
-#
+#%%
 # TODO: Use the code on the course's SciKit-Learn page to output a .DOT file
 # Then render the .DOT to .PNGs. Ensure you have graphviz installed.
 # If not, `brew install graphviz`. If you can't, use: http://webgraphviz.com/.
@@ -70,5 +82,13 @@ print "High-Dimensionality Score: ", round((score*100), 3)
 # tree directly from the exported tree.dot file without having to issue a call.
 #
 # .. your code here ..
+from sklearn.tree import export_graphviz
 
+with open("wheat.dot", 'w') as f:
+    f = export_graphviz(dt, out_file=f, class_names=['poisonous', 'edible'])
 
+#%%
+importances = {x: 0 for x in X.columns}
+
+for idx, weight in enumerate(dt.feature_importances_):
+    importances[X.columns[idx]] = weight
