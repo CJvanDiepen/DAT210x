c0b0af4618f32636db7952ccd21e8566b64bcf8d
CJvanDiepen <sjaak.vandiepen@tno.nl>
2017-05-31 15:55:38 +0200

    Finish last assignment.

--- Module6/assignment6.py
+++ Module6/assignment6.py
@@ -6,25 +6,26 @@ import time
 # http://groupware.les.inf.puc-rio.br/static/har/dataset-har-PUC-Rio-ugulino.zip
 
 
-#
+#%%
 # TODO: Load up the dataset into dataframe 'X'
 #
 # .. your code here ..
+X = pd.read_csv(r"C:\Users\diepencjv\repos\DAT210x\Module6\dataset-har-PUC-Rio-ugulino.csv", delimiter=';')
 
 
-
-#
+#%%
 # TODO: Encode the gender column, 0 as male, 1 as female
 #
 # .. your code here ..
-
+X['gender'] = X['gender'].map({'Man': 0,'Woman': 1})
 
 #
 # TODO: Clean up any column with commas in it
 # so that they're properly represented as decimals instead
 #
 # .. your code here ..
-
+for col in ['how_tall_in_meters', 'body_mass_index']:
+    X[col] = pd.to_numeric([x.replace(',', '.') for x in X[col]], errors='coerce')
 
 #
 # INFO: Check data types
@@ -32,29 +33,31 @@ print X.dtypes
 
 
 
-#
+#%%
 # TODO: Convert any column that needs to be converted into numeric
 # use errors='raise'. This will alert you if something ends up being
 # problematic
 #
 # .. your code here ..
-
+#X['z4'] = pd.to_numeric(X['z4'], errors='raise')
 
 #
 # INFO: If you find any problematic records, drop them before calling the
 # to_numeric methods above...
-
-
+X['z4'] = pd.to_numeric(X['z4'], errors='coerce')
+X = X.dropna(axis=0)
 #
 # TODO: Encode your 'y' value as a dummies version of your dataset's "class" column
 #
 # .. your code here ..
+y = pd.get_dummies(X['class'])
 
 
 #
 # TODO: Get rid of the user and class columns
 #
 # .. your code here ..
+X = X.drop(['user', 'class'], axis=1)
 print X.describe()
 
 
@@ -64,12 +67,14 @@ print X[pd.isnull(X).any(axis=1)]
 
 
 
-#
+#%%
 # TODO: Create an RForest classifier 'model' and set n_estimators=30,
 # the max_depth to 10, and oob_score=True, and random_state=0
 #
 # .. your code here ..
+from sklearn.ensemble import RandomForestClassifier
 
+model= RandomForestClassifier(n_estimators=30, max_depth=10, oob_score=True, random_state=0)
 
 
 # 
@@ -79,42 +84,46 @@ print X[pd.isnull(X).any(axis=1)]
 #
 # .. your code here ..
 
+from sklearn.model_selection import train_test_split
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.3, random_state=7)
 
-
-
+#%%
 print "Fitting..."
 s = time.time()
 #
 # TODO: train your model on your training set
 #
 # .. your code here ..
+model.fit(X_train, y_train)
+
 print "Fitting completed in: ", time.time() - s
 
 
-#
+#%%
 # INFO: Display the OOB Score of your data
 score = model.oob_score_
 print "OOB Score: ", round(score*100, 3)
 
 
 
-
+#%%
 print "Scoring..."
 s = time.time()
 #
 # TODO: score your model on your test set
 #
 # .. your code here ..
+score = model.score(X_test, y_test)
 print "Score: ", round(score*100, 3)
 print "Scoring completed in: ", time.time() - s
 
 
-#
+#%%
 # TODO: Answer the lab questions, then come back to experiment more
 
 
-#
+#%%
 # TODO: Try playing around with the gender column
 # Encode it as Male:1, Female:0
 # Try encoding it to pandas dummies
